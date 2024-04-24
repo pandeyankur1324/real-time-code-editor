67841c0b9362e668604fa09f306e270c54501467
Nischay Sharma <nischaysharma04@gmail.com>
2024-04-24 23:34:31 +0530

    compilex added

--- server.js
+++ server.js
@@ -1,66 +1,175 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const ACTIONS = require('./src/Actions');
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const ACTIONS = require("./src/Actions");
+const compiler = require("compilex");
+const bodyParser = require("body-parser");
+var options = { stats: true };
+compiler.init(options);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
+app.post("/compile", (req, res) => {
+  const { code, input, lang } = req.body;
+  try {
+    if (lang === "Cpp") {
+      const envData = { OS: "linux", cmd: "gcc", options: { timeout: 10000 } };
+      if (!input) {
+        compiler.compileCPP(envData, code, function (data) {
+          if (data.output) {
+            res.send(data);
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          } else {
+            res.send({ output: "error in code" });
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          }
+        });
+      } else {
+        compiler.compileCPPWithInput(envData, code, input, function (data) {
+          if (data.output) {
+            res.send(data);
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          } else {
+            res.send({ output: "error in code" });
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          }
+        });
+      }
+    } else if (lang === "Java") {
+      const envData = { OS: "linux", options: { timeout: 10000 } };
+      if (!input) {
+        compiler.compileJava(envData, code, function (data) {
+          if (data.output) {
+            res.send(data);
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          } else {
+            res.send({ output: "error in code" });
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          }
+        });
+      } else {
+        compiler.compileJavaWithInput(envData, code, input, function (data) {
+          if (data.output) {
+            res.send(data);
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          } else {
+            res.send({ output: "error in code" });
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          }
+        });
+      }
+    } else if (lang === "Python") {
+      const envData = { OS: "linux", options: { timeout: 10000 } };
+      if (!input) {
+        compiler.compilePython(envData, code, function (data) {
+          if (data.output) {
+            res.send(data);
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          } else {
+            res.send({ output: "error in code" });
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          }
+        });
+      } else {
+        compiler.compilePythonWithInput(envData, code, input, function (data) {
+          if (data.output) {
+            res.send(data);
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          } else {
+            res.send({ output: "error in code" });
+            compiler.flush(function () {
+              console.log("All temporary files flushed !");
+            });
+          }
+        });
+      }
+    }
+  } catch (e) {
+    console.log("err", e);
+  }
+});
+
+app.use(express.static("build"));
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
-    // Map
-    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
-        (socketId) => {
-            return {
-                socketId,
-                username: userSocketMap[socketId],
-            };
-        }
-    );
+  // Map
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
 }
 
-io.on('connection', (socket) => {
-    console.log('socket connected', socket.id);
+io.on("connection", (socket) => {
+  console.log("socket connected", socket.id);
 
-    socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
-        userSocketMap[socket.id] = username;
-        socket.join(roomId);
-        const clients = getAllConnectedClients(roomId);
-        clients.forEach(({ socketId }) => {
-            io.to(socketId).emit(ACTIONS.JOINED, {
-                clients,
-                username,
-                socketId: socket.id,
-            });
-        });
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectedClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
     });
+  });
 
-    socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
-        socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
+  socket.on(ACTIONS.CODE_CHANGE, ({ roomId, code }) => {
+    socket.in(roomId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
 
-    socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
-        io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
-    });
+  socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
+    io.to(socketId).emit(ACTIONS.CODE_CHANGE, { code });
+  });
 
-    socket.on('disconnecting', () => {
-        const rooms = [...socket.rooms];
-        rooms.forEach((roomId) => {
-            socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
-                socketId: socket.id,
-                username: userSocketMap[socket.id],
-            });
-        });
-        delete userSocketMap[socket.id];
-        socket.leave();
+  socket.on("disconnecting", () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
     });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
 });
 
 const PORT = process.env.PORT || 8000;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,135 +1,187 @@
-import React, { useState, useRef, useEffect } from 'react';
-import toast from 'react-hot-toast';
-import ACTIONS from '../Actions';
-import Client from '../components/Client';
-import Editor from '../components/Editor';
-import { initSocket } from '../socket';
+import React, { useState, useRef, useEffect } from "react";
+import toast from "react-hot-toast";
+import ACTIONS from "../Actions";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+import { initSocket } from "../socket";
 import {
-    useLocation,
-    useNavigate,
-    Navigate,
-    useParams,
-} from 'react-router-dom';
+  useLocation,
+  useNavigate,
+  Navigate,
+  useParams,
+} from "react-router-dom";
 
 const EditorPage = () => {
-    const socketRef = useRef(null);
-    const codeRef = useRef(null);
-    const location = useLocation();
-    const { roomId } = useParams();
-    const reactNavigator = useNavigate();
-    const [clients, setClients] = useState([]);
+  const socketRef = useRef(null);
+  const codeRef = useRef(null);
+  const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClients] = useState([]);
 
-    useEffect(() => {
-        const init = async () => {
-            socketRef.current = await initSocket();
-            socketRef.current.on('connect_error', (err) => handleErrors(err));
-            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+  const [code, setCode] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [textareaValue, setTextareaValue] = useState("");
 
-            function handleErrors(e) {
-                console.log('socket error', e);
-                toast.error('Socket connection failed, try again later.');
-                reactNavigator('/');
-            }
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+    console.log(selectedOption);
+  };
 
-            socketRef.current.emit(ACTIONS.JOIN, {
-                roomId,
-                username: location.state?.username,
-            });
+  const input = document.querySelector("#input");
 
-            // Listening for joined event
-            socketRef.current.on(
-                ACTIONS.JOINED,
-                ({ clients, username, socketId }) => {
-                    if (username !== location.state?.username) {
-                        toast.success(`${username} joined the room.`);
-                        console.log(`${username} joined`);
-                    }
-                    setClients(clients);
-                    socketRef.current.emit(ACTIONS.SYNC_CODE, {
-                        code: codeRef.current,
-                        socketId,
-                    });
-                }
-            );
+  const submit = async () => {
+    const editorCode = {
+      code: code,
+      input: input.value,
+      lang: selectedOption,
+    };
+    const oData = await fetch("http://localhost:8000/compile", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editorCode),
+    });
+    var d = await oData.json();
+    setTextareaValue(d.output);
+  };
 
-            // Listening for disconnected
-            socketRef.current.on(
-                ACTIONS.DISCONNECTED,
-                ({ socketId, username }) => {
-                    toast.success(`${username} left the room.`);
-                    setClients((prev) => {
-                        return prev.filter(
-                            (client) => client.socketId !== socketId
-                        );
-                    });
-                }
-            );
-        };
-        init();
-        return () => {
-            socketRef.current.disconnect();
-            socketRef.current.off(ACTIONS.JOINED);
-            socketRef.current.off(ACTIONS.DISCONNECTED);
-        };
-    }, []);
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
 
-    async function copyRoomId() {
-        try {
-            await navigator.clipboard.writeText(roomId);
-            toast.success('Room ID has been copied to your clipboard');
-        } catch (err) {
-            toast.error('Could not copy the Room ID');
-            console.error(err);
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      // Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
         }
-    }
+      );
 
-    function leaveRoom() {
-        reactNavigator('/');
-    }
+      // Listening for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
+    };
+    init();
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    };
+  }, []);
 
-    if (!location.state) {
-        return <Navigate to="/" />;
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success("Room ID has been copied to your clipboard");
+    } catch (err) {
+      toast.error("Could not copy the Room ID");
+      console.error(err);
     }
+  }
 
-    return (
-        <div className="mainWrap">
-            <div className="aside">
-                <div className="asideInner">
-                    <div className="logo">
-                        <img
-                            className="logoImage"
-                            src="/code-sync.png"
-                            alt="logo"
-                        />
-                    </div>
-                    <h3>Connected</h3>
-                    <div className="clientsList">
-                        {clients.map((client) => (
-                            <Client
-                                key={client.socketId}
-                                username={client.username}
-                            />
-                        ))}
-                    </div>
-                </div>
-                <button className="btn copyBtn" onClick={copyRoomId}>
-                    Copy ROOM ID
-                </button>
-                <button className="btn leaveBtn" onClick={leaveRoom}>
-                    Leave
-                </button>
-            </div>
-            <div className="editorWrap">
-                <Editor
-                    socketRef={socketRef}
-                    roomId={roomId}
-                    onCodeChange={(code) => {
-                        codeRef.current = code;
-                    }}
-                />
-            </div>
+  function leaveRoom() {
+    reactNavigator("/");
+  }
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-sync.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
         </div>
-    );
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>
+          Leave
+        </button>
+      </div>
+      <div className="editorWrap">
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+            setCode(code);
+          }}
+        />
+      </div>
+      <div className="rightSide">
+        <div className="selectionContainer">
+          <button className="runBtn" onClick={submit}>
+            Run
+          </button>
+          <select
+            id="options"
+            value={selectedOption}
+            onChange={handleDropdownChange}
+          >
+            <option className="opt" Selected>
+              Choose
+            </option>
+            <option className="opt" value="Cpp">
+              C++
+            </option>
+            {/* <option className="opt"  value="Java">Java</option> */}
+            <option className="opt" value="Python">
+              Python
+            </option>
+          </select>
+        </div>
+        <div className="inputField">
+          <h3>Input</h3>
+          <textarea className="rightTextArea" id="input"></textarea>
+        </div>
+        <div className="outputField">
+          <h3>Output</h3>
+          <textarea
+            className="rightTextArea"
+            id="output"
+            value={textareaValue}
+          ></textarea>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EditorPage;
